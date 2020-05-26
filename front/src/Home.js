8b5879dbae4aab2f0c8255a39fc07612a37d17b6
Felipe Silva <felipealvessilva64@gmail.com>
2020-05-26 17:29:21 -0300

    fixing identation to space 👌

--- front/src/Home.js
+++ front/src/Home.js
@@ -6,24 +6,24 @@ import gameLogoB from './assets/memorize-it.png'
 import gameLogoE from './assets/star-wars-game.png'
 
 function Home() {
-	return (
-		<Fragment>
-			<div className='logo-system'>
-				<Image 
-					src={gameLogoB.valueOf()} 
-					alt='main system logo'
-					objectFit="cover" />
-				<Image 
-					src={gameLogoE.valueOf()} 
-					alt='main system second logo'
-					objectFit="cover" />
-			</div>
-			<ButtonGroup m={5} spacing={4}>
-				<Login />
-				<Register />
-			</ButtonGroup>
-		</Fragment>
-	)
+  return (
+    <Fragment>
+      <div className='logo-system'>
+        <Image
+          src={gameLogoB.valueOf()}
+          alt='main system logo'
+          objectFit="cover" />
+        <Image
+          src={gameLogoE.valueOf()}
+          alt='main system second logo'
+          objectFit="cover" />
+      </div>
+      <ButtonGroup m={5} spacing={4}>
+        <Login />
+        <Register />
+      </ButtonGroup>
+    </Fragment>
+  )
 }
 
 export default Home
